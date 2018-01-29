47818cf7678bf6b5e204adb4fe0233d16cd0dec7
liuyanglg@outlook.com <j00jpg809>
2018-01-29 21:24:13 +0800

    vue simple project

---
+++ src/components/SayHello.vue
@@ -0,0 +1,23 @@
+<template>
+<div id="say-hello">
+  <h1>{{msg}} say hello to you!</h1>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "say-hello",
+      data(){
+          return{
+            msg:'Misty Snow'
+          }
+      }
+    }
+</script>
+
+<style scoped>
+  h1{
+    color: #31b77f;
+  }
+
+</style>
